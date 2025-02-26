b273747a0faa067d4c9a083a0260403056f1e694
DylanGlass <glasd6702@gmail.com>
2025-02-26 14:12:10 -0500

    add procedural music looping

--- Assets/Scripts/AudioManger.cs
+++ Assets/Scripts/AudioManger.cs
@@ -1,71 +1,159 @@
 using UnityEngine;
-using UnityEngine.UI;
+using System;
+using Unity.VisualScripting;
+using System.Collections;
 
 public class AudioManager : MonoBehaviour {
     public static AudioManager instance;
 
-    public AudioClip clip;
-    public AudioSource musicSource;
-    public AudioSource[] sfxSources;
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource[] _musicSources;
+    public int musicSourceIndex = 0;
+    public AudioSource sfxSource;
 
-    private void Start() {
-        PlayMusic(clip);
-    }
+    public double musicDuration;
+    public double goalTime;
 
     private const string MusicVolumeKey = "MusicVolume";
     private const string SFXVolumeKey = "SFXVolume";
-    private void Awake() {
-        if (instance == null) {
+
+    private System.Random random = new System.Random();
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
             instance = this;
-            DontDestroyOnLoad(gameObject); // Persist between scenes
+            DontDestroyOnLoad(gameObject);
         }
-        else {
+        else
+        {
             Destroy(gameObject);
+        }
+
+    }
+
+    private void Start()
+    {
+        PlayMusic(musicSounds[0].name);
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Music: " + name + " not found!");
             return;
         }
-
-        LoadVolumeSettings();
+        goalTime = AudioSettings.dspTime + 0.5;
+        _musicSources[0].clip = s.clip;
+        _musicSources[0].PlayScheduled(goalTime);
+        musicDuration = (double)_musicSources[musicSourceIndex].clip.samples / _musicSources[musicSourceIndex].clip.frequency;
+        goalTime += musicDuration;
     }
 
-    public void PlayMusic(AudioClip clip) {
-        if (musicSource.clip != clip) {
-            musicSource.clip = clip;
-            musicSource.Play();
+    private void Update()
+    {
+        if (AudioSettings.dspTime > goalTime - 1)
+        {
+            PlayScheduledClip();
         }
     }
 
-    public void StopMusic() {
-        musicSource.Stop();
+    private void PlayScheduledClip()
+    {
+        Sound s = Array.Find(musicSounds, sound => sound.name == NextTrack());
+        _musicSources[musicSourceIndex].clip = s.clip;
+        _musicSources[musicSourceIndex].PlayScheduled(goalTime);
+        musicDuration = (double)_musicSources[musicSourceIndex].clip.samples / _musicSources[musicSourceIndex].clip.frequency;
+        goalTime += musicDuration;
+        musicSourceIndex = 1 - musicSourceIndex;
     }
 
-    public void PlaySFX(AudioClip clip) {
-        foreach (var source in sfxSources) {
-            if (!source.isPlaying) // Play on the first available source
-            {
-                source.clip = clip;
-                source.Play();
-                return;
-            }
+    public string NextTrack()
+    {
+        int index;
+
+        if (musicSounds.Length == 1)
+        {
+            return musicSounds[0].name;
         }
+        index = random.Next(0, musicSounds.Length-1);
+        return musicSounds[index].name;
     }
+
+    public void SetCurrentMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, sound => sound.name == NextTrack());
+        _musicSources[musicSourceIndex].clip = s.clip;
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("SFX: " + name + " not found!");
+            return;
+        }
+        sfxSource.PlayOneShot(s.clip);
+    }
+
+
+    //private void Awake() {
+    //    if (instance == null) {
+    //        instance = this;
+    //        DontDestroyOnLoad(gameObject); // Persist between scenes
+    //    }
+    //    else {
+    //        Destroy(gameObject);
+    //        return;
+    //    }
+
+    //    LoadVolumeSettings();
+    //}
+
+    //public void PlayMusic(AudioClip clip) {
+    //    if (musicSource.clip != clip) {
+    //        musicSource.clip = clip;
+    //        musicSource.Play();
+    //    }
+    //}
+
+    //public void StopMusic() {
+    //    musicSource.Stop();
+    //}
+
+    //public void PlaySFX(AudioClip clip) {
+    //    foreach (var source in sfxSources) {
+    //        if (!source.isPlaying) // Play on the first available source
+    //        {
+    //            source.clip = clip;
+    //            source.Play();
+    //            return;
+    //        }
+    //    }
+    //}
 
     public void SetMusicVolume(float volume) {
-        musicSource.volume = volume;
+        foreach (var source in _musicSources)
+        {
+            source.volume = volume;
+        }
         PlayerPrefs.SetFloat(MusicVolumeKey, volume);
     }
 
     public void SetSFXVolume(float volume) {
-        foreach (var source in sfxSources) {
-            source.volume = volume;
-        }
+        sfxSource.volume = volume;
         PlayerPrefs.SetFloat(SFXVolumeKey, volume);
     }
 
-    private void LoadVolumeSettings() {
-        musicSource.volume = PlayerPrefs.GetFloat(MusicVolumeKey, VolumeSettings.DEFAULT_MUSIC_VOLUME);
-        foreach (var source in sfxSources) {
-            source.volume = PlayerPrefs.GetFloat(SFXVolumeKey, VolumeSettings.DEFAULT_SFX_VOLUME);
-        }
-    }
+    //private void LoadVolumeSettings() {
+    //    musicSource.volume = PlayerPrefs.GetFloat(MusicVolumeKey, VolumeSettings.DEFAULT_MUSIC_VOLUME);
+    //    foreach (var source in sfxSources) {
+    //        source.volume = PlayerPrefs.GetFloat(SFXVolumeKey, VolumeSettings.DEFAULT_SFX_VOLUME);
+    //    }
+    //}
 
 }
