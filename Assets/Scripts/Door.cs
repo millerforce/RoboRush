2929bbe47207ec6ca45513143e65dddefbe09f1b
P-William <w.paetz@uzwelo.com>
2025-02-26 22:16:51 -0500

    Hint Manager

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -11,16 +11,28 @@ public class Door : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        print("door trigger entered");
         if (other.CompareTag("Player"))
         {
             if (Input.GetKey(KeyCode.E)) 
             {
+                InteractionHintManager.instance.HideHint();
                 PlayerPrefs.SetInt("Day", _dayToStart);
                 PlayerPrefs.Save();
 
                 SceneManager.LoadScene(_levelToLoad);
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            InteractionHintManager.instance.ShowHint();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            InteractionHintManager.instance.HideHint();
         }
     }
 }
